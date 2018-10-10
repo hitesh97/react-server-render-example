1a561177fb6449d86880674f378b09b7669b5a56
Hitesh Khatri <hitesh.khatri@microlease.com>
2018-10-10 11:17:39 +0100

    formatting

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
-import Helmet from "react-helmet";
-import {Switch, Route} from 'react-router-dom';
-import {Link, NavLink} from 'react-router-dom';
+import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+import { Switch, Route } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import styles from '../styles/styles.pcss';
 
 class Menu extends Component {
@@ -10,13 +10,22 @@ class Menu extends Component {
             <div>
                 <ul>
                     <li>
-                        <NavLink exact to={'/'} activeClassName={styles.active}>Homepage</NavLink>
+                        <NavLink exact to={'/'} activeClassName={styles.active}>
+                            Homepage
+                        </NavLink>
                     </li>
                     <li>
-                        <NavLink activeClassName={styles.active} to={'/about'}>About</NavLink>
+                        <NavLink activeClassName={styles.active} to={'/about'}>
+                            About
+                        </NavLink>
                     </li>
                     <li>
-                        <NavLink activeClassName={styles.active} to={'/contact'}>Contact</NavLink>
+                        <NavLink
+                            activeClassName={styles.active}
+                            to={'/contact'}
+                        >
+                            Contact
+                        </NavLink>
                     </li>
                 </ul>
             </div>
@@ -24,14 +33,12 @@ class Menu extends Component {
     }
 }
 
-
 class Homepage extends Component {
-
-    render () {
+    render() {
         return (
             <div className={styles.component}>
-                <Helmet title="Welcome to our Homepage"/>
-                <Menu/>
+                <Helmet title="Welcome to our Homepage" />
+                <Menu />
                 <h1>Homepage</h1>
             </div>
         );
@@ -39,11 +46,11 @@ class Homepage extends Component {
 }
 
 class About extends Component {
-    render () {
+    render() {
         return (
             <div>
-                <Helmet title="About us"/>
-                <Menu/>
+                <Helmet title="About us" />
+                <Menu />
                 <h1>About</h1>
             </div>
         );
@@ -51,37 +58,41 @@ class About extends Component {
 }
 
 class Contact extends Component {
-    render () {
+    render() {
         return (
             <div>
-                <Helmet title="Contact us"/>
-                <Menu/>
+                <Helmet title="Contact us" />
+                <Menu />
                 <h1>Contact</h1>
             </div>
         );
     }
 }
 
-
 export default class App extends Component {
-
-    render () {
+    render() {
         return (
             <div>
                 <Helmet
-                    htmlAttributes={{lang: "en", amp: undefined}} // amp takes no value
+                    htmlAttributes={{ lang: 'en', amp: undefined }} // amp takes no value
                     titleTemplate="%s | React App"
-                    titleAttributes={{itemprop: "name", lang: "en"}}
+                    titleAttributes={{ itemprop: 'name', lang: 'en' }}
                     meta={[
-                        {name: "description", content: "Server side rendering example"},
-                        {name: "viewport", content: "width=device-width, initial-scale=1"},
+                        {
+                            name: 'description',
+                            content: 'Server side rendering example'
+                        },
+                        {
+                            name: 'viewport',
+                            content: 'width=device-width, initial-scale=1'
+                        }
                     ]}
-                    link={[{rel: "stylesheet", href: "/dist/styles.css"}]}
+                    link={[{ rel: 'stylesheet', href: '/public/styles.css' }]}
                 />
                 <Switch>
-                    <Route exact path='/' component={Homepage}/>
-                    <Route path='/about' component={About}/>
-                    <Route path='/contact' component={Contact}/>
+                    <Route exact path="/" component={Homepage} />
+                    <Route path="/about" component={About} />
+                    <Route path="/contact" component={Contact} />
                 </Switch>
             </div>
         );
